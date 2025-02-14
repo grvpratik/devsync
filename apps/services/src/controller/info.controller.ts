c9817c8a8236fbc1798b109bc7f9d607fc7b04f5
grvPratik <pratikgauravv@gmail.com>
2025-02-14 21:34:21 +0000

    core 🔧: cleanup

--- apps/services/src/controller/info.controller.ts
+++
@@ -1,7 +0,0 @@
-export async function InfoController(idea: string, c: any) {
-	return {
-		name: "test",
-		
-		description: "lol this is thr description of the page",
-	};
-}
--- apps/services/src/routes/personalRoute.ts
+++
@@ -1,4 +0,0 @@
-import { Hono } from "hono";
-
-
-export const personal = new Hono();
